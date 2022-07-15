872641b79563d800827806b725733cb793af0c38
zmh <zmh13201594014@163.com>
2022-07-15 23:44:08 +0800

    "hot-fix commit"

--- src/main/java/com/ht/GitTest.java
+++ src/main/java/com/ht/GitTest.java
@@ -11,6 +11,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hot-fix commit");
     }
 }
