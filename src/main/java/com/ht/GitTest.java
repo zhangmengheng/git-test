8761325c9b519446551a6a765d26e6d3e47ede1b
zmh <zmh13201594014@163.com>
2022-07-15 22:59:08 +0800

    first commit

---
+++ src/main/java/com/ht/GitTest.java
@@ -0,0 +1,12 @@
+package com.ht;
+
+/**
+ * @Author zmh
+ * @Date 2022/7/15 22:55
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
